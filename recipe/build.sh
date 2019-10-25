2438a4626779ad430279e18c3e63fe6559ca9099
Jonathan Helmus <jjhelmus@gmail.com>
2019-10-25 13:51:18 -0500

    enable pcre
    * enable pcre
    * reorganize requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-export C_INCLUDE_PATH="${PREFIX}/include"
-export LIBRARY_PATH="${PREFIX}/lib"
-
-if [[ ${HOST} =~ .*darwin.* ]]; then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${LIBRARY_PATH}"
-elif [[ ${HOST} =~ .*linux.* ]]; then
-    export CFLAGS="${CFLAGS} -I${C_INCLUDE_PATH}"
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${LIBRARY_PATH} -L${LIBRARY_PATH}"
-fi
-
 # NO_GETTEXT disables internationalization (localized message translations)
 # NO_INSTALL_HARDLINKS uses symlinks which makes the package 85MB slimmer (8MB instead of 93MB!)
+
+# pcre-feedstock recipe does not include --enable-jit
+export NO_LIBPCRE1_JIT=1
 
 # Add a place for git config files.
 mkdir -p "${PREFIX}/etc"
@@ -21,6 +14,7 @@ make configure
     --host=${HOST} \
     --with-gitattributes="${PREFIX}/etc/gitattributes" \
     --with-gitconfig="${PREFIX}/etc/gitconfig" \
+    --with-libpcre1 \
     --with-iconv="${PREFIX}/lib" \
     --with-perl="${PREFIX}/bin/perl" \
     --with-tcltk="${PREFIX}/bin/tclsh"
