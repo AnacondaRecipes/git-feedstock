122d71c8f684be480463ce7db48ea06bfeeae462
Kai Tietz <ktietz@anaconda.com>
2021-08-13 14:11:43 +0200

    update to 2.32.0, and add arm64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,42 +1,71 @@
 #!/bin/bash
 
-# NO_GETTEXT disables internationalization (localized message translations)
+set -x
+
 # NO_INSTALL_HARDLINKS uses symlinks which makes the package 85MB slimmer (8MB instead of 93MB!)
 
-# pcre-feedstock recipe does not include --enable-jit
-export NO_LIBPCRE1_JIT=1
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 && "$target_platform" == "osx-arm64" ]]; then
+  export ac_cv_iconv_omits_bom=no
+  export ac_cv_fread_reads_directories=yes
+  export ac_cv_snprintf_returns_bogus=no
+fi
+
+pushd code
 
 # Add a place for git config files.
-mkdir -p "${PREFIX}/etc"
+mkdir -p $PREFIX/etc
 make configure
 ./configure \
     --prefix="${PREFIX}" \
-    --host=${HOST} \
     --with-gitattributes="${PREFIX}/etc/gitattributes" \
     --with-gitconfig="${PREFIX}/etc/gitconfig" \
-    --with-libpcre1 \
+    --with-libpcre \
     --with-iconv="${PREFIX}/lib" \
     --with-perl="${PREFIX}/bin/perl" \
     --with-tcltk="${PREFIX}/bin/tclsh"
 make \
-    --jobs=${CPU_COUNT} \
-    NO_GETTEXT=1 \
+    --jobs="$CPU_COUNT" \
     NO_INSTALL_HARDLINKS=1 \
-    all strip install \
-    ${VERBOSE_AT}
+    STRIP=$STRIP \
+    all strip install
 
 # build osxkeychain
-if [[ $(uname) == "Darwin" ]]; then
+if [[ "$target_platform" == osx-* ]]; then
   pushd contrib/credential/osxkeychain
   make -e
   cp -avf git-credential-osxkeychain $PREFIX/bin
   popd
 fi
 
-git config --system http.sslVerify true
-git config --system http.sslCAPath "${PREFIX}/ssl/cacert.pem"
-git config --system http.sslCAInfo "${PREFIX}/ssl/cacert.pem"
+if [[ -z "${REQUESTS_CA_BUNDLE}" ]] 
+then
+    cert_file="${PREFIX}/ssl/cacert.pem"
+else
+    cert_file="${REQUESTS_CA_BUNDLE}"
+fi
+
+mkdir -p $PREFIX/etc
+echo "[http]"                            >  $PREFIX/etc/gitconfig
+echo "        sslVerify = true"          >> $PREFIX/etc/gitconfig
+echo "        sslCAPath = ${cert_file}"  >> $PREFIX/etc/gitconfig
+echo "        sslCAInfo = ${cert_file}"  >> $PREFIX/etc/gitconfig
 
 # Install completion files
 mkdir -p $PREFIX/share/bash-completion/completions
 cp contrib/completion/git-completion.bash $PREFIX/share/bash-completion/completions/git
+
+popd # code
+
+# Install manpages
+mkdir -p $PREFIX/man
+cp -r manpages/* $PREFIX/man
+# Add symlinks in $PREFIX/share/man so that manpages work on macOS
+if [[ $(uname) == "Darwin" ]]; then
+  ln -s $PREFIX/man/man1/git* $PREFIX/share/man/man1/
+  ln -s $PREFIX/man/man5/git* $PREFIX/share/man/man5/
+  ln -s $PREFIX/man/man7/git* $PREFIX/share/man/man7/
+fi
+
+# Install htmldocs
+mkdir -p $PREFIX/share/doc/git
+cp -r htmldocs/* $PREFIX/share/doc/git
