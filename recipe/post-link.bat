885ea7b7bdc674d7b3952661540167be45786db7
Marco Esters <mesters@anaconda.com>
2024-06-03 11:18:44 -0700

    Use CONDA_PYTHON_EXE to get menuinst version

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -3,8 +3,10 @@
 REM The menuinst v2 json file is not compatible with menuinst versions
 REM older than 2.1.0. Copy the appropriate file as the menu file.
 
+SET LOGFILE=%PREFIX%\.messages.txt
+
 FOR /F %%i IN (
-    'conda run -n base python -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))"'
+    '%CONDA_PYTHON_EXE% -c "import menuinst; print(tuple(int(x) for x in menuinst.__version__.split(\".\"))[:3] < (2, 1, 0))"'
 ) DO (
     IF "%%~i"=="True" GOTO :use_menuinst_v1
 )
@@ -13,8 +15,10 @@ GOTO :exit
 
 :use_menuinst_v1:
     COPY /y %PREFIX%\Menu\%PKG_NAME%_menu-v1.json.bak %PREFIX%\Menu\%PKG_NAME%_menu.json
-    ECHO "Warning: using menuinst v1 shortcuts"
-    ECHO "Please update menuinst in the base environment and reinstall %PKG_NAME%."
+    ECHO. >> %LOGFILE%
+    ECHO Warning: using menuinst v1 shortcuts. >> %LOGFILE%
+    ECHO menuinst v1 is marked as legacy and is no longer maintained. >> %LOGFILE%
+    ECHO Please update menuinst in the base environment and reinstall %PKG_NAME%. >> %LOGFILE%
     GOTO :exit
 
 :exit
