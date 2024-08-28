a8c87d7cfaa1bc34a4a17480c843fe98959af559
Marco Esters <mesters@anaconda.com>
2024-08-28 16:25:00 -0700

    Add comments

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -7,17 +7,19 @@ SET LOGFILE=%PREFIX%\.messages.txt
 SET MENU_DIR=%PREFIX%\Menu
 SET MENU_PATH=%MENU_DIR%\%PKG_NAME%_menu.json
 
+REM Determine menuinst version.
+REM menuinst in the base environment is used to create the shortcuts,
+REM so use the python binary in the base environment.
 IF EXIST "%CONDA_PYTHON_EXE%" (
     SET PYTHON_CMD="%CONDA_PYTHON_EXE%"
     GOTO :get_menuinst
 )
-
+REM The CONDA_PYTHON_EXE variable is not set for installers, so use conda-standalone.
 IF EXIST "%PREFIX%\_conda.exe" (
     SET PYTHON_CMD="%PREFIX%\_conda.exe" python
     SET CONDA_STANDALONE=1
     GOTO :get_menuinst
 )
-
 GOTO :use_menuinst_v1
 
 :get_menuinst
@@ -25,6 +27,8 @@ GOTO :use_menuinst_v1
 IF %ERRORLEVEL% == 1 GOTO :use_menuinst_v1
 
 COPY /y "%PREFIX%\Menu\%PKG_NAME%_menu-v2.json.bak" "%MENU_PATH%"
+
+REM Determine if the shortcut is installed into the base environment
 %PYTHON_CMD% -c "import os, sys; from pathlib import Path; from menuinst.utils import _default_prefix; sys.exit(int(Path(os.environ['PREFIX']).samefile(_default_prefix(which='base'))))"
 IF %ERRORLEVEL% == 0 (
     CALL :patch "__ENV_PLACEHOLDER__= ^({{ ENV_NAME }}^)"
